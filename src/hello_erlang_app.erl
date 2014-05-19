5464dd8002ac5c6c3440844370aafb0310c0f759
Julian Wachholz <julian@wachholz.ch>
2014-05-19 19:19:44 +0200

    Custom error handling is always nice.

--- src/hello_erlang_app.erl
+++ src/hello_erlang_app.erl
@@ -13,10 +13,29 @@ start(_Type, _Args) ->
         {"/echo_post", echo_post_handler, []}
     ]}]),
     {ok, _} = cowboy:start_http(http, 100, [{port, 8080}], [
-        {env, [{dispatch, Dispatch}]}
+        {env, [{dispatch, Dispatch}]},
+        {onresponse, fun error_hook/4}
     ]),
     hello_erlang_sup:start_link().
 
 
 stop(_State) ->
     ok.
+
+
+error_hook(404, Headers, <<>>, Req) ->
+    {Path, Req2} = cowboy_req:path(Req),
+    Body = ["404 Not Found: \"", Path,
+            "\" is not the path you're looking for.\n"],
+    Headers2 = lists:keyreplace(<<"content-length">>, 1, Headers,
+        {<<"content-length">>, integer_to_list(iolist_size(Body))}),
+    {ok, Req3} = cowboy_req:reply(404, Headers2, Body, Req2),
+    Req3;
+error_hook(Code, Headers, <<>>, Req) when is_integer(Code), Code >= 400 ->
+    Body = ["HTTP Error ", integer_to_list(Code), $\n],
+    Headers2 = lists:keyreplace(<<"content-length">>, 1, Headers,
+        {<<"content-length">>, integer_to_list(iolist_size(Body))}),
+    {ok, Req2} = cowboy_req:reply(Code, Headers2, Body, Req),
+    Req2;
+error_hook(_Code, _Headers, _Body, Req) ->
+    Req.
