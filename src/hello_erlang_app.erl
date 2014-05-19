b9dbe3a24289d5da1a564391973d7b17b6e2ac71
Julian Wachholz <julian@wachholz.ch>
2014-05-19 19:04:38 +0200

    Add a simple echo handler, too.

--- src/hello_erlang_app.erl
+++ src/hello_erlang_app.erl
@@ -4,15 +4,18 @@
 -export([start/2]).
 -export([stop/1]).
 
+
 start(_Type, _Args) ->
     Dispatch = cowboy_router:compile([{'_', [
         {"/", hello_handler, []},
-        {"/chunked", chunked_handler, []}
+        {"/chunked", chunked_handler, []},
+        {"/echo", echo_handler, []}
     ]}]),
     {ok, _} = cowboy:start_http(http, 100, [{port, 8080}], [
         {env, [{dispatch, Dispatch}]}
     ]),
     hello_erlang_sup:start_link().
+
 
 stop(_State) ->
     ok.
